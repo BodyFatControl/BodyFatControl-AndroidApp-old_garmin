fb0de020e268091ceeb70b8b5b8a5dc872ba1e55
casainho <casainho@gmail.com>
2016-12-14 12:44:42 +0000

    New activity added for listing Garmin available devices. User selects from a list and the device is saved on a SharePreferences, this way at app start user is not asked to select the Garmin device if it is already selected

---
+++ app/src/main/java/comdailyweightfatcontrol/httpsgithub/dailyfatcontrol_androidapp/ConnectActivity.java
@@ -0,0 +1,165 @@
+package comdailyweightfatcontrol.httpsgithub.dailyfatcontrol_androidapp;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.garmin.android.connectiq.ConnectIQ;
+import com.garmin.android.connectiq.IQDevice;
+import com.garmin.android.connectiq.exception.InvalidStateException;
+import com.garmin.android.connectiq.exception.ServiceUnavailableException;
+import com.google.gson.Gson;
+
+import comdailyweightfatcontrol.httpsgithub.dailyfatcontrol_androidapp.adapter.IQDeviceAdapter;
+
+import java.util.List;
+
+public class ConnectActivity extends ListActivity {
+
+    private ConnectIQ mConnectIQ;
+    private TextView mEmptyView;
+    private IQDeviceAdapter mAdapter;
+    private boolean mSdkReady = false;
+
+    private ConnectIQ.IQDeviceEventListener mDeviceEventListener = new ConnectIQ.IQDeviceEventListener() {
+
+        @Override
+        public void onDeviceStatusChanged(IQDevice device, IQDevice.IQDeviceStatus status) {
+            mAdapter.updateDeviceStatus(device, status);
+        }
+
+    };
+
+    private ConnectIQ.ConnectIQListener mListenerSDKInitialize = new ConnectIQ.ConnectIQListener() {
+
+        @Override
+        public void onInitializeError(ConnectIQ.IQSdkErrorStatus errStatus) {
+            if( null != mEmptyView )
+                mEmptyView.setText(R.string.initialization_error + errStatus.name());
+            mSdkReady = false;
+        }
+
+        @Override
+        public void onSdkReady() {
+            loadDevices();
+            mSdkReady = true;
+        }
+
+        @Override
+        public void onSdkShutDown() {
+            mSdkReady = false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connect);
+
+        mAdapter = new IQDeviceAdapter(this);
+        getListView().setAdapter(mAdapter);
+
+        // Here we are specifying that we want to use a WIRELESS bluetooth connection.
+        // We could have just called getInstance() which would by default create a version
+        // for WIRELESS, unless we had previously gotten an instance passing TETHERED
+        // as the connection type.
+        mConnectIQ = ConnectIQ.getInstance(this, ConnectIQ.IQConnectType.WIRELESS);
+
+        // Initialize the SDK
+        mConnectIQ.initialize(this, true, mListenerSDKInitialize);
+
+        mEmptyView = (TextView)findViewById(android.R.id.empty);
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (mSdkReady) {
+            loadDevices();
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        // It is a good idea to unregister everything and shut things down to
+        // release resources and prevent unwanted callbacks.
+        try {
+            mConnectIQ.unregisterAllForEvents();
+            mConnectIQ.shutdown(this);
+        } catch (InvalidStateException e) {
+            // This is usually because the SDK was already shut down
+            // so no worries.
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.load_devices) {
+            loadDevices();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        IQDevice device = mAdapter.getItem(position);
+
+        // save the device object (Garmin device) on SharedPreferences
+        SharedPreferences.Editor prefsEditor = getSharedPreferences("MainSharedPreferences", MODE_PRIVATE).edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(device);
+        prefsEditor.putString("garmin_device", json);
+        prefsEditor.commit();
+
+        finish();
+    }
+
+    public void loadDevices() {
+        // Retrieve the list of known devices
+        try {
+            List<IQDevice> devices = mConnectIQ.getKnownDevices();
+
+            if (devices != null) {
+                mAdapter.setDevices(devices);
+
+                // Let's register for device status updates.  By doing so we will
+                // automatically get a status update for each device so we do not
+                // need to call getStatus()
+                for (IQDevice device : devices) {
+                    mConnectIQ.registerForDeviceEvents(device, mDeviceEventListener);
+                }
+            }
+
+        } catch (InvalidStateException e) {
+            // This generally means you forgot to call initialize(), but since
+            // we are in the callback for initialize(), this should never happen
+        } catch (ServiceUnavailableException e) {
+            // This will happen if for some reason your app was not able to connect
+            // to the ConnectIQ service running within Garmin Connect Mobile.  This
+            // could be because Garmin Connect Mobile is not installed or needs to
+            // be upgraded.
+            if( null != mEmptyView )
+                mEmptyView.setText(R.string.service_unavailable);
+        }
+    }
+
+}
--- app/src/main/java/comdailyweightfatcontrol/httpsgithub/dailyfatcontrol_androidapp/MainActivity.java
+++ app/src/main/java/comdailyweightfatcontrol/httpsgithub/dailyfatcontrol_androidapp/MainActivity.java
@@ -1,8 +1,12 @@
 package comdailyweightfatcontrol.httpsgithub.dailyfatcontrol_androidapp;
 
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +16,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import comdailyweightfatcontrol.httpsgithub.dailyfatcontrol_androidapp.adapter.IQDeviceAdapter;
+import com.garmin.android.connectiq.ConnectIQ;
+import com.garmin.android.connectiq.ConnectIQ.ConnectIQListener;
+import com.garmin.android.connectiq.ConnectIQ.IQConnectType;
+import com.garmin.android.connectiq.ConnectIQ.IQDeviceEventListener;
+import com.garmin.android.connectiq.ConnectIQ.IQSdkErrorStatus;
+import com.garmin.android.connectiq.IQApp;
+import com.garmin.android.connectiq.IQDevice;
+import com.garmin.android.connectiq.IQDevice.IQDeviceStatus;
+import com.garmin.android.connectiq.exception.InvalidStateException;
+import com.garmin.android.connectiq.exception.ServiceUnavailableException;
+import com.garmin.android.connectiq.ConnectIQ.IQApplicationEventListener;
+
+import com.google.gson.Gson;
+
+import java.util.List;
+
+import static comdailyweightfatcontrol.httpsgithub.dailyfatcontrol_androidapp.DeviceActivity.MY_APP;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ConnectIQ mConnectIQ;
+    private IQDevice mIQDevice;
+    private IQDeviceAdapter mAdapter;
+    private boolean mSdkReady = false;
+    private IQApp mConnectIQApp = null;
+    public static final String MY_APP = "a3421feed289106a538cb9547ab12095";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +63,108 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // verifiy if Garmin device is saved on SharedPreferences
+        SharedPreferences mPrefs = getSharedPreferences("MainSharedPreferences", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = mPrefs.getString("garmin_device", "");
+        IQDevice mIQDevice = gson.fromJson(json, IQDevice.class);
+
+        if (mIQDevice != null) {
+            // Get our instance of ConnectIQ.  Since we initialized it
+            // in our MainActivity, there is no need to do so here, we
+            // can just get a reference to the one and only instance.
+            mConnectIQ = ConnectIQ.getInstance();
+            try {
+                mConnectIQ.registerForDeviceEvents(mIQDevice, new IQDeviceEventListener() {
+
+                    @Override
+                    public void onDeviceStatusChanged(IQDevice device, IQDeviceStatus status) {
+                        // Since we will only get updates for this device, just display the status
+//                        mDeviceStatus.setText(status.name());
+                    }
+
+                });
+            } catch (InvalidStateException e) {
+//                Log.wtf(TAG, "InvalidStateException:  We should not be here!");
+            }
+
+            // Let's check the status of our application on the device.
+            try {
+                mConnectIQ.getApplicationInfo(MY_APP, mIQDevice, new ConnectIQ.IQApplicationInfoListener() {
+
+                    @Override
+                    public void onApplicationInfoReceived(IQApp app) {
+
+                        mConnectIQApp = app;
+
+                        // This is a good thing. Now we can show our list of message options.
+//                        String[] options = getResources().getStringArray(R.array.send_message_display);
+//                        String[] options = null;
+//
+//                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(DeviceActivity.this, android.R.layout.simple_list_item_1, options);
+//                        setListAdapter(adapter);
+
+                    }
+
+                    @Override
+                    public void onApplicationNotInstalled(String applicationId) {
+                        // The Comm widget is not installed on the device so we have
+                        // to let the user know to install it.
+                        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                        dialog.setTitle(R.string.missing_widget);
+                        dialog.setMessage(R.string.missing_widget_message);
+                        dialog.setPositiveButton(android.R.string.ok, null);
+                        dialog.create().show();
+                    }
+
+                });
+            } catch (InvalidStateException e1) {
+            } catch (ServiceUnavailableException e1) {
+            }
+
+            // Let's register to receive messages from our application on the device.
+            try {
+                mConnectIQ.registerForAppEvents(mIQDevice, mConnectIQApp, new IQApplicationEventListener() {
+
+                    @Override
+                    public void onMessageReceived(IQDevice device, IQApp app, List<Object> message, ConnectIQ.IQMessageStatus status) {
+
+                        // We know from our Comm sample widget that it will only ever send us strings, but in case
+                        // we get something else, we are simply going to do a toString() on each object in the
+                        // message list.
+                        StringBuilder builder = new StringBuilder();
+
+                        if (message.size() > 0) {
+                            for (Object o : message) {
+                                builder.append(o.toString());
+                                builder.append("\r\n");
+                            }
+                        } else {
+                            builder.append("Received an empty message from the application");
+                        }
+
+                        AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                        dialog.setTitle(R.string.received_message);
+                        dialog.setMessage(builder.toString());
+                        dialog.setPositiveButton(android.R.string.ok, null);
+                        dialog.create().show();
+                    }
+                });
+            } catch (InvalidStateException e) {
+                Toast.makeText(this, "ConnectIQ is not in a valid state", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            // Since there is no IQDevice saved on SharedPreferences, start the activity for user
+            // select the IQDevice
+            Intent intent = new Intent(this, ConnectActivity.class);
+            startActivity(intent);
+        }
     }
 
     @Override
@@ -73,6 +207,9 @@ public class MainActivity extends AppCompatActivity
 
         if (id == R.id.connect) {
             // Handle the connect action
+            Intent intent = new Intent(this, ConnectActivity.class);
+            startActivity(intent);
+
         } else if (id == R.id.about) {
 
         }
