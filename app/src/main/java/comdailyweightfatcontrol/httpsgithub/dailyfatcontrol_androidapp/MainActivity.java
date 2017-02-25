e82daf8c16a9aba4c3e5d65b26bf67494d907d02
casainho <casainho@gmail.com>
2017-02-25 19:22:36 +0000

    Added drawer layout.

--- app/src/main/java/comdailyweightfatcontrol/httpsgithub/dailyfatcontrol_androidapp/MainActivity.java
+++ app/src/main/java/comdailyweightfatcontrol/httpsgithub/dailyfatcontrol_androidapp/MainActivity.java
@@ -6,14 +6,20 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.style.ForegroundColorSpan;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -67,16 +73,13 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
-
-//public class MainActivity extends AppCompatActivity
-//        implements NavigationView.OnNavigationItemSelectedListener {
 public class MainActivity extends AppCompatActivity implements OnChartValueSelectedListener,
-        OnChartGestureListener {
+        OnChartGestureListener, NavigationView.OnNavigationItemSelectedListener {
 
     private Tracker mTracker;
     private ConnectIQ mConnectIQ;
     private IQDevice mIQDevice;
-    private IQDeviceAdapter mAdapter;
+    private IQDeviceAdapter iqDeviceAdapter;
     private boolean mSdkReady = false;
     public static final String MY_APP = "3F3D83A85F584671A551EA1316623AD7";
     private IQApp mConnectIQApp = new IQApp(MY_APP);
@@ -109,6 +112,8 @@ public class MainActivity extends AppCompatActivity implements OnChartValueSelec
     private DataBaseUserProfile mDataBaseUserProfile = null;
     private UserProfile mUserProfile = null;
     private Context mContext;
+
+    NavigationView navigationView;
 
     public static SharedPreferences getPrefs() {
         return Prefs;
@@ -349,6 +354,19 @@ public class MainActivity extends AppCompatActivity implements OnChartValueSelec
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            this.getWindow().setStatusBarColor(Color.BLACK);
+        }
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         mContext = getApplication().getApplicationContext();
 
         // [START shared_tracker]
@@ -443,6 +461,31 @@ public class MainActivity extends AppCompatActivity implements OnChartValueSelec
                 return true;
             }
         });
+    }
+
+    //@SuppressWarnings("StatementWithEmptyBody")
+    //@Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        if (id == R.id.ic_menu_home){
+
+        } else if (id == R.id.ic_menu_connect){
+            // Handle the connect action
+            Intent intent = new Intent(this, ConnectActivity.class);
+            startActivity(intent);
+
+            mTracker.send(new HitBuilders.EventBuilder()
+                    .setCategory("Action")
+                    .setAction("ConnectActivity")
+                    .build());
+        } else if (id == R.id.ic_menu_about){
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
